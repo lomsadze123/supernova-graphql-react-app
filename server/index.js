a24857dbf1ae26094a8c4e8523986ab71827aa89
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-05-17 20:56:40 +0400

    continue on enviroment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
@@ -8,6 +10,24 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+const prisma = new PrismaClient();
+
+async function fetchUsers() {
+  try {
+    const users = await prisma.user.findMany();
+    console.log("Users:", users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+fetchUsers().catch((error) => {
+  console.error("Error occurred during application startup:", error);
+  process.exit(1); // Exit the application with an error code
 });
 
 app.listen(process.env.PORT || 3001, () =>
