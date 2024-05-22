3dfa6b2a79c73edc91ae9a8f2f3d13ef11fbded5
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-05-22 16:52:44 +0400

    change for production. (forgot)

--- client/src/utils/apolloClient.ts
+++ client/src/utils/apolloClient.ts
@@ -5,13 +5,13 @@ import { getMainDefinition } from "@apollo/client/utilities";
 const DEV_MODE = "development";
 
 const httpLink = new HttpLink({
-  uri: DEV_MODE
+  uri: !DEV_MODE
     ? "http://localhost:3001/graphql"
     : "https://supernova-graphql-react-app.onrender.com/graphql",
 });
 
 const wsLink = new WebSocketLink({
-  uri: DEV_MODE
+  uri: !DEV_MODE
     ? "ws://localhost:3001/graphql"
     : "wss://supernova-graphql-react-app.onrender.com/graphql",
   options: {
